b10a7feb39e37ce43fdcb6773a8546c1a57b76ef
Jimmy Al Ma <jimmyalma2121@gmail.com>
2026-06-30 07:12:19 +0700

    day15- project movie search done

--- Challenge Project/Two/script.js
+++ Challenge Project/Two/script.js
@@ -3,22 +3,24 @@ const category = document.getElementById('category')
 const loadingText = document.getElementById('loadingText')
 const movieContainer = document.getElementById('movieContainer')
 
+const defaultSearch = 'star'
+
+let response
+
 async function loadMovie() {
     try {
-        const APIkey = '1407fd32'
-        const defaultSearch = 'star'
-        
-        let url = `https://www.omdbapi.com/?apikey=${APIkey}&`
-        
-        let response
+        loadingText.style.display = 'flex'
 
-        if (category.value === 'default') {
-            response = await fetch(url + `s=${defaultSearch}`)
-            
-        } else {
-            response = await fetch(url + `s=${category.value}`)
+        const keyword = searchBar.value.trim().toLowerCase()
+        const finalKeyword = keyword ? keyword : defaultSearch
+
+        let url = `https://www.omdbapi.com/?apikey=${CONFIG.API_KEY}&s=$${finalKeyword}`
+
+        if (category.value !== 'default') {
+            url += `&type=${category.value}`
         }
 
+        const response = await fetch(url)
         const data = await response.json()
 
         loadingText.style.display = 'none'
@@ -37,14 +39,16 @@ function renderMovie(movieData) {
     movieContainer.innerHTML = ''
     movieData.forEach(movie => {
         movieContainer.innerHTML += ` 
-            <div style="width: 200px; height: 300px; border: 2px solid black">
-                <h2>${movie.Title}</h2>
+            <div style="width: 300px; height: 400px; border: 2px solid black; display: flex; flex-direction: column; align-items: center;">
+                <h3>${movie.Title}</h3>
                 <h3>${movie.Year}</h3>
-                <h3>${movie.Type}</h3>
-                <h4>${movie.imdbID}</h4>
+                <img src="${movie.Poster}" style="width: 75%; height: 250px">
             </div>
         `
     })
 }
+
+searchBar.addEventListener('input', loadMovie)
+category.addEventListener('change', loadMovie)
 
 loadMovie()
