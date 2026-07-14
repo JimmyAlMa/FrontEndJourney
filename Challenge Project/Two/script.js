7bfcf46a4d1525da54232f056906aebe335c47ce
Jimmy Al Ma <jimmyalma2121@gmail.com>
2026-07-14 15:56:56 +0700

    feat: add modal to display movie description on card click

--- Challenge Project/Two/script.js
+++ Challenge Project/Two/script.js
@@ -16,7 +16,7 @@ async function loadMovie() {
         const keyword = searchBar.value.trim().toLowerCase()
         const finalKeyword = keyword ? keyword : defaultSearch
 
-        let url = `https://www.omdbapi.com/?apikey=${CONFIG.MOVIE_API_KEY}&s=$${finalKeyword}`
+        let url = `https://www.omdbapi.com/?apikey=${CONFIG.MOVIE_API_KEY}&s=${finalKeyword}`
 
         if (category.value !== 'default') {
             url += `&type=${category.value}`
@@ -45,9 +45,48 @@ function renderMovie(movieData) {
                 <h3>${movie.Title}</h3>
                 <h3>${movie.Year}</h3>
                 <img src="${movie.Poster}" style="width: 75%; height: 250px">
+                <button class="detailButton" data-id="${movie.imdbID}">See detail</button>
             </div>
         `
+
+        const allDetailButton = document.querySelectorAll('.detailButton')
+        allDetailButton.forEach(button => {
+            button.addEventListener('click', async function(event) {
+                const imdbID = event.target.dataset.id
+                console.log('Getting id:' + imdbID)
+                await showMovieDetail(imdbID)
+            })
+        })
     })
+}
+
+
+async function showMovieDetail(id) {
+    try {
+        const res = await fetch(`https://www.omdbapi.com/?apikey=${CONFIG.MOVIE_API_KEY}&i=${id}`)
+        const movie = await res.json()
+
+        if (movie.Response === 'True') {
+            renderMovieDetail(movie)
+        }
+    } catch (err) {
+        console.log('Error:' + err)
+    }
+}
+
+function renderMovieDetail(movie) {
+    movieContainer.innerHTML = ''
+    movieContainer.innerHTML += `
+        <div>
+            <img src="${movie.Poster}" alt="${movie.Title}">
+            <h2>${movie.Title} (${movie.Year})</h2>
+            <h3>Genre: ${movie.Genre}</h3>
+            <h3>Director: ${movie.Director}</h3>
+            <h3>Actor: ${movie.Actors}</h3>
+            <h3>Plot: ${movie.Plot}</h3>
+            <h3>Rating: ${movie.imdbRating}</h3>
+        </div>
+    `
 }
 
 function changeTheme() {
@@ -59,6 +98,7 @@ function changeTheme() {
         themeButton.textContent = 'Dark Mode'
     }
 }
+
 
 searchBar.addEventListener('input', loadMovie)
 category.addEventListener('change', loadMovie)
